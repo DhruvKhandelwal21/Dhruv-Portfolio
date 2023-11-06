5dc077ecb2ac79a825d99080a8aaa85c0ab427ea
dhruv <dhruv21.dk@gmail.com>
2023-11-06 19:45:57 +0530

    fix

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { isMobile } from "react-device-detect";
 import CanvasLoader from "../Loader";
-const Computers = (isMobile: any) => {
+const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
@@ -58,12 +59,14 @@ const computerCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
+        {!isMobile && (
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+        )}
+        <Computers />
       </Suspense>
       <Preload all />
     </Canvas>
